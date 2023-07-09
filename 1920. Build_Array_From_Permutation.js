522a4170e5d6d4ea92229f13aa285fada903343d
RedwanKibria <86778079+RedwanKibria@users.noreply.github.com>
2023-07-09 22:36:44 +0600

    Submitted solution

---
+++ 1920. Build_Array_From_Permutation.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+    var output = [];
+    for(var i = 0; i < nums.length; i++){
+        output [i] = nums[nums[i]];
+    }
+
+    return output;
+};
