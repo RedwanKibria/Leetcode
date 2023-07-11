93883a0db62f73f33abe005e9990ec876a0153fb
RedwanKibria <86778079+RedwanKibria@users.noreply.github.com>
2023-07-12 01:29:15 +0600

    Submitted solution

---
+++ 1929. Concatenation_Of_Array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+  var firstArray = nums;
+  var secondArray = nums;
+  var result = firstArray.concat(secondArray);
+
+  return result;  
+};
